b628b57e58008ceab7a8377c45b73d99076404cf
Igal Klebanov <igalklebanov@gmail.com>
2022-10-17 02:02:34 +0300

    add relate query node.

--- src/operation-node/operation-node.ts
+++ src/operation-node/operation-node.ts
@@ -1,9 +1,15 @@
-export type SurrealOperationNodeKind = 'CreateQueryNode' | 'ReturnNode'
+export type SurrealOperationNodeKind = 'CreateQueryNode' | 'ReturnNode' | 'RelateQueryNode'
 
 export interface SurrealOperationNode {
   readonly kind: SurrealOperationNodeKind
 }
 
+const surrealKindDictionary: Record<SurrealOperationNodeKind, true> = {
+  CreateQueryNode: true,
+  RelateQueryNode: true,
+  ReturnNode: true,
+}
+
 export function isSurrealOperationNode(node: {kind: string}): node is SurrealOperationNode {
-  return node.kind === 'CreateQueryNode' || node.kind === 'ReturnNode'
+  return surrealKindDictionary[node.kind as keyof typeof surrealKindDictionary] === true
 }
---
+++ src/operation-node/relate-query-node.ts
@@ -0,0 +1,52 @@
+import type {ColumnUpdateNode, TableNode, ValueNode} from 'kysely'
+
+import type {VertexNode} from '../parser/vertex-expression-parser.js'
+import {freeze} from '../util/object-utils.js'
+import type {SurrealOperationNode} from './operation-node.js'
+import type {ReturnNode} from './return-node.js'
+
+export interface RelateQueryNode extends SurrealOperationNode {
+  readonly kind: 'RelateQueryNode'
+  readonly from: VertexNode
+  readonly relation?: TableNode
+  readonly to?: VertexNode
+  readonly content?: ValueNode
+  readonly set?: ReadonlyArray<ColumnUpdateNode>
+  readonly return?: ReturnNode
+}
+
+export const RelateQueryNode = freeze({
+  is(node: SurrealOperationNode): node is RelateQueryNode {
+    return node.kind === 'RelateQueryNode'
+  },
+
+  create(from: VertexNode, relation: TableNode, to: VertexNode): RelateQueryNode {
+    return freeze({
+      kind: 'RelateQueryNode',
+      from,
+      relation,
+      to,
+    })
+  },
+
+  cloneWithContent(relateQuery: RelateQueryNode, content: ValueNode): RelateQueryNode {
+    return freeze({
+      ...relateQuery,
+      content,
+    })
+  },
+
+  cloneWithSet(relateQuery: RelateQueryNode, set: ReadonlyArray<ColumnUpdateNode>): RelateQueryNode {
+    return freeze({
+      ...relateQuery,
+      set: relateQuery.set ? freeze([...relateQuery.set, ...set]) : set,
+    })
+  },
+
+  cloneWithReturn(relateQuery: RelateQueryNode, returnNode: ReturnNode): RelateQueryNode {
+    return freeze({
+      ...relateQuery,
+      return: returnNode,
+    })
+  },
+})
