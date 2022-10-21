416989a8c80df7708f986b561145fabf4b620f44
Igal Klebanov <igalklebanov@gmail.com>
2022-10-22 02:34:13 +0300

    restrict type of id in records.

--- src/util/surreal-types.ts
+++ src/util/surreal-types.ts
@@ -1,14 +1,20 @@
+import type {Generated} from 'kysely'
+
 export type SurrealDatabase<DB> = {
   [K in keyof DB | SurrealRecordId<DB>]: K extends `${infer TB}:${string}`
     ? TB extends keyof DB
-      ? DB[TB]
+      ? SurrealRecord<DB[TB]>
       : never
     : K extends keyof DB
-    ? DB[K]
+    ? SurrealRecord<DB[K]>
     : never
 }
 
 export type SurrealRecordId<DB> = keyof DB extends string ? `${keyof DB}:${string}` : never
+
+export type SurrealRecord<R> = {
+  [C in 'id' | keyof R]: C extends 'id' ? Generated<string> : C extends keyof R ? R[C] : never
+}
 
 export type AnyTable<DB, TB extends keyof DB = keyof DB> = TB extends `${infer T}:${string}`
   ? T
