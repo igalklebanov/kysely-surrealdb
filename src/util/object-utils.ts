71b9c81c9c15eb5fa7c872eb2febae1d55abe57e
Igal Klebanov <igalklebanov@gmail.com>
2022-10-23 01:42:27 +0300

    ...

--- src/util/object-utils.ts
+++ src/util/object-utils.ts
@@ -1,3 +1,7 @@
 export function freeze<T>(obj: T): Readonly<T> {
   return Object.freeze(obj)
 }
+
+export function isReadonlyArray(obj: unknown): obj is ReadonlyArray<any> {
+  return Array.isArray(obj)
+}
