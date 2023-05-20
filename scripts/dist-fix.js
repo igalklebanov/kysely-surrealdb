b3175e274131ef276e70d382176e282b6d3854d9
Igal Klebanov <igalklebanov@gmail.com>
2023-05-20 03:52:08 +0300

    add helpers to build process.

--- scripts/dist-fix.js
+++ scripts/dist-fix.js
@@ -1,45 +1,64 @@
-const {mkdir, readdir, rename, rm, writeFile, copyFile, readFile, unlink} = require('node:fs/promises')
+const {mkdir, readdir, rename, rm, writeFile, copyFile, readFile, unlink, move} = require('fs-extra')
 const path = require('node:path')
 
 ;(async () => {
   const distPath = path.join(__dirname, '../dist')
   const distCjsPath = path.join(distPath, 'cjs')
   const distEsmPath = path.join(distPath, 'esm')
+  const distEsmHelpersPath = path.join(distEsmPath, 'helpers')
+  const distHelpersPath = path.join(distPath, 'helpers')
 
-  const [dist, distEsm] = await Promise.all([
+  const [distSubpaths, distEsmSubpaths, distEsmHelpersSubpaths, distHelpersSubpaths] = await Promise.all([
     readdir(distPath),
     readdir(distEsmPath),
+    readdir(distEsmHelpersPath),
+    readdir(distHelpersPath),
     rm(distCjsPath, {force: true, recursive: true}),
   ])
 
   await Promise.all([
     mkdir(distCjsPath),
-    writeFile(path.join(distEsmPath, 'package.json'), JSON.stringify({type: 'module', sideEffects: false})),
-    ...dist
-      .filter((distFilePath) => distFilePath.match(/\.d\.ts$/))
-      .map((distFilePath) => copyFile(path.join(distPath, distFilePath), path.join(distEsmPath, distFilePath))),
-    ...distEsm
-      .filter((esmFilePath) => esmFilePath.match(/\.js$/))
-      .map(async (esmFilePath) => {
-        const distEsmFilePath = path.join(distEsmPath, esmFilePath)
-
-        const esmFile = await readFile(distEsmFilePath)
-        const esmFileContents = esmFile.toString()
-
-        const dtsFilePath = `./${esmFilePath.replace('.js', '.d.ts')}`
-
-        const denoFriendlyEsmFileContents = [`/// <reference types="${dtsFilePath}" />`, esmFileContents].join('\n')
-
-        await unlink(distEsmFilePath)
-
-        await writeFile(distEsmFilePath, denoFriendlyEsmFileContents)
-      }),
+    writePackageJsonFile(distEsmPath, 'module'),
+    ...copyDtsFiles(distPath, distSubpaths, distEsmPath),
+    ...copyDtsFiles(distHelpersPath, distHelpersSubpaths, distEsmHelpersPath),
+    ...addReferenceTypesTripleDash(distEsmPath, distEsmSubpaths),
+    ...addReferenceTypesTripleDash(distEsmHelpersPath, distEsmHelpersSubpaths),
   ])
 
   await Promise.all([
-    writeFile(path.join(distCjsPath, 'package.json'), JSON.stringify({type: 'commonjs', sideEffects: false})),
-    ...dist
+    writePackageJsonFile(distCjsPath, 'commonjs'),
+    ...distSubpaths
       .filter((filePath) => filePath.match(/\.[t|j]s(\.map)?$/))
       .map((filePath) => rename(path.join(distPath, filePath), path.join(distCjsPath, filePath))),
+    move(distHelpersPath, path.join(distCjsPath, 'helpers')),
   ])
 })()
+
+function writePackageJsonFile(destinationPath, type) {
+  return writeFile(path.join(destinationPath, 'package.json'), JSON.stringify({type, sideEffects: false}))
+}
+
+function copyDtsFiles(sourcePath, sourceSubpaths, destinationPath) {
+  return sourceSubpaths
+    .filter((sourceSubpath) => sourceSubpath.match(/\.d\.ts$/))
+    .map((dtsFilename) => copyFile(path.join(sourcePath, dtsFilename), path.join(destinationPath, dtsFilename)))
+}
+
+function addReferenceTypesTripleDash(folderPath, folderContentPaths) {
+  return folderContentPaths
+    .filter((contentPath) => contentPath.match(/\.js$/))
+    .map(async (filename) => {
+      const filePath = path.join(folderPath, filename)
+
+      const file = await readFile(filePath)
+      const fileContents = file.toString()
+
+      const dtsFilePath = `./${filename.replace('.js', '.d.ts')}`
+
+      const denoFriendlyFileContents = [`/// <reference types="${dtsFilePath}" />`, fileContents].join('\n')
+
+      await unlink(filePath)
+
+      await writeFile(filePath, denoFriendlyFileContents)
+    })
+}
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import {defineConfig} from 'tsup'
 export default defineConfig({
   clean: true,
   dts: true,
-  entry: ['./src/index.ts'],
+  entry: ['./src/index.ts', './src/helpers/index.ts'],
   format: ['cjs', 'esm'],
   legacyOutput: true,
   outDir: 'dist',
